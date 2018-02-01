07919fa7af570465813f0d046f6a0851c648effc
Xiaoqaing Liu <shunjiheng@gmail.com>
2018-02-01 16:45:54 +0800

    add 2 files

---
+++ road_connect.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+This file is used to accomplish the road tracing based extend kalman filter
+Created on Jan 08 2017
+
+!Note: we use the row and col to represent the road coordinate rather than x y,
+       the theta is calculated using xOy coordinate. 
+@author: Xiaoqiang Liu
+"""
+
+# calculate the state of start point
+from road_kalman import RoadEKF
+import numpy as np
+
+def vector_direction(v):
+    """ Returns the direction angle of vectors v1::
+
+            >>> vector_direction((1, 0))
+            0.0
+            >>> angle_between((1, 1)
+    """
+    
+	base = np.array([1, 0])
+    v = v / np.linalg.norm(v)
+    return np.sign(v[1]) * np.arccos(np.clip(np.dot(v, base), -1.0, 1.0))
+ 
+def get_line(skeleton):
+	#the fuction need to return the road
+	#using hough transform to detect line in binary_image, the direction can be
+	#get form the vector calculation
+	 
+	lines = cv2.HoughLinesP(sk.astype(np.uint8),rho=1,theta=np.pi/180,threshold=100,
+                minLineLength=minLineLength,maxLineGap=maxLineGap)
+		
+	return lines
+
+def get_profile(image, r, c, theta, road_width=10):
+    # we use row and col coordinate
+	src = np.array([r - road_width * np.cos(theta), c + road_width * np.sin(theta)])
+    dst = np.array([r + road_width * np.cos(theta), c - road_width * np.sin(theta)])
+    profile = skimage.measure.profile_line(image, src, dst, linewidth=3, order=0, mode='constant', cval=0.0)
+    return profile
+	
+def match_profile(profile, Set_profile):
+	# using least square get the shift between the two profile
+	corrcoef = numpy.corrcoef(profile, Set_Profile)
+	measuredData = np.array(yvalues['int1'])
+	calibrationData = np.array(yvalues['int0'])
+
+	A = np.vstack( [measuredData, np.ones(len(measuredData))]).T
+	gain,offset = np.linalg.lstsq(A, calibrationData)[0]
+	return corrcoef, gain, offset
+	
+def main():
+    
+	road = []
+    dt = 3
+    road_tracker = RoadEKF(dt)
+    road_tracker.P = np.array(4*4)    # uncertainty covariance
+    road_tracker.Q = np.array(4*4)    # process uncertainty
+    road_tracker.R = np.array(4*4)    # state uncertainty
+
+    for line in lines:
+	    subroad = []
+		for x1,y1,x2,y2 in line:
+			vector12 = np.array([x1-x2, y1-y2])
+			subroad.append([x1, y1])
+			road_tracker.x = np.array([[x1, y1, vector_direction(vector12), 0]])
+			profile = get_profile(image, x1, y1, vector_direction(vector12))
+			i = 0
+			while(True):
+			    road_tracker.predict()
+				predict_x = road_tracker.x
+				z, profile = get_measurment(predict_x, profile) #the function has not implement
+				road_tracker.update(z)
+				subroad.append([road_tracker.x[0,0], road_tracker.x[1,0]])
+			
+			
+			vector21 = np.array([x2-x1, y2-y1])
+			subroad.append([x2, y2])
+			road_tracker.x = np.array([[x2, y2, vector_direction(vector21), 0]])
+			
+			profile = get_profile(image, x2, y2, vector_direction(vector21))
+			i = 0
+			while(True):
+			    road_tracker.predict()
+				predict_x = road_tracker.x
+				z, profile = get_measurment(predict_x, profile) #the function has not implement
+				road_tracker.update(z)
+				subroad.insert(0, [road_tracker.x[0,0], road_tracker.x[1,0]])
+		road.append(subroad)
+    
+	
+if __name__ == '__main__':
+    main()
---
+++ road_kalman.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Jan 29 2017
+@author: Xiaoqiang Liu
+"""
+
+
+import numpy as np
+from numpy import dot, array, sqrt
+import sympy
+from sympy import symbols, Matrix
+from sympy.abc import r, c, theta, beta
+from filterpy.kalman import ExtendedKalmanFilter as EKF 
+
+
+class RoadEKF(EKF):
+	def __init__(self, dt):
+		EKF.__init__(self, 4, 2)
+		r, c , theta, beta = symbols('r, c, theta, beta') # beta is the change of theta
+		self.dt = dt
+		self.fxu = Matrix([[r - dt*sin(theta + beta*dt/2)],
+			[c + dt*cos(theta + beta*dt/2)],
+			[theta + dt*beta],
+			[beta]])
+			
+		self.F_j = self.fxu.jacobian(Matrix([x, y, theta, beta]))
+		self.subs = {r:0, c:0, theta:0, beta:0}
+		self.r, self.c, self.theta, self.beta = r, c, theta, beta
+		
+	def predict(self, u=0):
+		
+		# predict the next state
+		dx = np.array([[self.dt*sin(x[2,0] + x[3,0]*dt/2.0)],
+			[self.dt*cos(x[2,0] + x[3,0]*dt/2.0)],
+			[x[3,0]*self.dt],
+			[0]])
+		
+		self.x += dx
+		
+		self.subs[self.r] = self.x[0,0]
+		self.subs[self.c] = self.x[1,0]
+		self.subs[self.theta] = self.x[2,0]
+		self.subs[self.beta] = self.beta[3,0]
+		F = array(self.F_j.evalf(subs = self.subs)).astype(float)	
+		
+		self.P = np.dot(F, self.P).dot(F.T) + self.Q
+		
+	def update(self, z):
+	    
+		P = self.P
+		R = self.R
+		if np.isscalar(z) and self.dim_z == 1:
+		    z = np.asarray([z], float)
+		
+		x = self.x
+		H = self.H
+		PHT = dot(P, H.T)
+		S = dot(H, PHT) + R
+		
+		self.K = PHT.dot(linalg.inv (S))
+		self.y = z - np.dot(H, x)
+		self.x = x + dot(self.K, self.y)
+		
+		I_KH = self._I - dot(self.K, H)
+		self.P = dot(I_KH, P).dot(I_KH.T) + dot(self.K, R).dot(self.K.T)
+		
+		
+		
